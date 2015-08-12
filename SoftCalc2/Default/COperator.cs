a6c408cb3119afa349bff5c07b0e30ed76f19c54
IMRinat <shamrinat@mail.ru>
2015-08-12 23:20:46 +0500

    commit4

--- SoftCalc2/Default/COperator.cs
+++ SoftCalc2/Default/COperator.cs
@@ -5,7 +5,6 @@ namespace SoftCalc2.Default
 {
 	public class COperator:IOperator
 	{
-
 		public COperator(string name, int priority, EnumOperatorType opertyp, Action<int, IContext> calcOper)
 		{
 			OperatorText = name;
@@ -17,7 +16,6 @@ namespace SoftCalc2.Default
         public string OperatorText { get; private set; }
         public int OperatorPriority { get; private set; }
         public EnumOperatorType OperatorType { get; private set; }
-		public Action<int, IContext> CalcOperator { get; private set; }
-
+		public Action<int, IContext>  CalcOperator { get; private set; }
 	}
 }
